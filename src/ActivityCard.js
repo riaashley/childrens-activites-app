a4caeb4dbc5a78c09fd797455d83499ce346ab3d
Ria Legaspi <riaveluz@gmail.com>
2022-12-10 22:03:52 -0800

    add name

--- src/ActivityCard.js
+++ src/ActivityCard.js
@@ -1,11 +1,12 @@
 import React from "react";
 
 function ActivityCard({ activity }) {
-    const { image } = activity
+    const { image, name} = activity
     return (
         <div className="activity-card">
             <img src={image} alt="photo" width="75%"
             />
+            <h1>{name}</h1>
         </div>
     )
 }

992888b45ccf68394575337e99559998865171f3
Ria Legaspi <riaveluz@gmail.com>
2022-12-10 23:01:33 -0800

    image input

--- src/AddForm.js
+++ src/AddForm.js
@@ -2,15 +2,23 @@ import React, { useState } from "react";
 
 function AddForm() {
     const [name, setName ] = useState("");
+    const [image, setImage] = useState("");
 
     function handleNameChange(e) {
         setName(e.target.value)
+    }
+
+    function handleImageChange(e) {
+        setImage(e.target.value)
     }
 
     return (
         <form className="add-activity">
             <label>Name
                 <input type="text" onChange={handleNameChange} value={name} />
+            </label>
+            <label>Image
+                <input type="text" onChange={handleImageChange} value={image} />
             </label>
             <button type="submit">Submit</button>
         </form>

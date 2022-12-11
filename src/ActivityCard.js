9e04172be0e266c1364725af574de2bcd84cf98c
Ria Legaspi <riaveluz@gmail.com>
2022-12-10 22:32:26 -0800

    delete button

--- src/ActivityCard.js
+++ src/ActivityCard.js
@@ -2,11 +2,19 @@ import React from "react";
 
 function ActivityCard({ activity }) {
     const { image, name} = activity
+
+    function handleDelete(){
+        
+    }
+
     return (
         <div className="activity-card">
             <img src={image} alt="photo" width="75%"
             />
             <h1>{name}</h1>
+            <button onClick={handleDelete}>
+                Remove
+            </button>
         </div>
     )
 }

4228cba185cec9c3eca3ccaab50e857aea05cc59
Ria Legaspi <riaveluz@gmail.com>
2022-12-10 21:54:36 -0800

    GET array of activities

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Home from "./Home";
 import ActivitiesContainer from "./ActivitiesContainer";
 import AddForm from "./AddForm";
 
 function App() {
+  const[activities, setActivities] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:3004/activities")
+      .then((r) => r.json())
+      .then(setActivities)
+  }, []);
+console.log(activities)
+
   return(
     <div>
       <Home />
